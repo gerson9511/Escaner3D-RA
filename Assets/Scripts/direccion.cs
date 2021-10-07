7fd2ed2523099262bb6f3808b028d9006cea8439
Gerson Jasan Rhu Botelho <45986551+gerson9511@users.noreply.github.com>
2021-10-06 23:00:58 -0400

    Add files via upload

---
+++ Assets/Scripts/direccion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class direccion : MonoBehaviour
+{
+     
+     public string URL;
+
+     private void OnMouseDown()
+     {
+         Application.OpenURL(URL);
+     }
+}
